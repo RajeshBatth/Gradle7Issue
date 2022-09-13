96e8c8995dec2406ca3ce945e71b3501871d1790
Rajesh Batth <rajesh.batth@go-mmt.com>
2022-09-13 21:08:26 +0530

    initial setup

---
+++ android/app/src/main/java/com/gradle7issue/MainActivity.java
@@ -0,0 +1,15 @@
+package com.gradle7issue;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "Gradle7Issue";
+  }
+}
